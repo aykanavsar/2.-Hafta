c9aec1ee6c2746ab88b2b625a96c0c17f1255766
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-13 13:02:00 +0300

    Add project files.

---
+++ fordonguleri/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] sayilar = { 1, 8, 3, 2, 7, 4 };
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.WriteLine($"eleman {sayilar[i]}");
+}
+
---
+++ fordonguleri2/Program.cs
@@ -0,0 +1,9 @@
+﻿int sayi = 5;
+int faktoriyel = 1;
+
+for (int i = 1; i <= sayi; i++)
+{
+    faktoriyel *= i;
+}
+Console.WriteLine($"Faktoriyel {faktoriyel}");
+
