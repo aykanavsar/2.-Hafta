e855bdb4d1b2033f2d168510ed8aef087e68a7d1
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-13 17:20:24 +0300

    odev3

---
+++ DoWhile/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+do
+{
+    //
+}
+while (koşul);
+*/
+
+int secim = 0;
+do
+{
+    Console.WriteLine("1. seçenek");
+    Console.WriteLine("2. seçenek");
+    Console.WriteLine("0. çıkış");
+
+    Console.WriteLine("bir seçim yapın");
+
+    secim = int.Parse(Console.ReadLine());
+
+    switch (secim)
+    {
+        case 1:
+            Console.WriteLine("1 seçtiniz");
+            break;
+
+        case 2:
+            Console.WriteLine("2 seçtiniz");
+            break;
+        case 0:
+
+            Console.WriteLine("çıkış seçildi");
+            break;
+        default:
+            Console.WriteLine("geçersiz");
+            break;
+
+    }
+
+}
+while (secim != 0);
+
+Console.WriteLine("çıkış yapıldı...");
+
---
+++ whilevsDowhileOdev/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata;
+
+int sayac = 0;
+Console.WriteLine("Lütfen bir limit değer giriniz :");
+int Limit = int.Parse(Console.ReadLine());
+while (sayac <= Limit)
+{
+  
+    Console.WriteLine("Ben bir Patika' lıyım");
+    sayac++;
+}
